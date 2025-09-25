c99702dd928719dda594a18a545f3ecf4276d967
Jayod Silva <dilharasilva05@gmail.com>
2025-09-25 12:57:32 +0530

    final img

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1304,7 +1304,7 @@ export default function Home() {
           >
 
             <img
-              src={SkynetPro}
+              src={Popup}
               alt="Special Offer"
               className="w-full h-auto object-contain rounded-lg"
             />
